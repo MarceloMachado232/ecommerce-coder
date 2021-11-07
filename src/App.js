05831a2d14167b531da455643bf2a02493d71d60
Machado Marcelo <marcelo.machado232@gmail.com>
2021-11-07 13:20:16 -0300

    Nav bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css'
+import {NavBar} from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+    <div className="my-app">
+
+    <NavBar/>
+
+    <h2>Bienvenidos</h2>
+
+      <section>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          lorem ipsum
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
+
+
+
+
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+export const NavBar = () => {
+
+return (
+
+<div className="header">
+        
+<h1>LOGO</h1>
+
+<nav>
+  
+  <ul>
+    
+    <li>HOME</li>
+    
+    <li>PRODUCTOS</li>
+    
+    <li>SUCURSALES</li>
+    
+    <li>NOSOTROS</li>
+
+  </ul>
+
+</nav>
+
+</div>
+)
+}
+
+
